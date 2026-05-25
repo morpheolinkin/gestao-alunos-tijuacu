4db2b1bcf84f188955a0f46384220197ef1b13a7
Jefferson <jeffesonmedeirosdasilva@gmail.com>
2026-05-25 09:03:36 -0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/test/java/br/com/tijuacu/gestaoalunos/GestaoAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tijuacu.gestaoalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAlunosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
